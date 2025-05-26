0009eb6ad8bd3fc73bb363bb0f86464e80a7b5c3
Lem Ticman <lemticman@Lems-MacBook-Pro.local>
2025-05-26 14:34:14 +0800

    Expose Flask app to 0.0.0.0 for Railway

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def delete_vehicle(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
