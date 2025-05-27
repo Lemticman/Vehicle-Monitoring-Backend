414b406385c2dbde97042c90934618f2b06caccf
Lem Ticman <lemticman@Lems-MacBook-Pro.local>
2025-05-27 15:56:15 +0800

    Add Trip model to models.py

--- models.py
+++ models.py
@@ -8,3 +8,16 @@ class Vehicle(db.Model):
     model = db.Column(db.String(100), nullable=False)
     year = db.Column(db.String(4), nullable=False)
     mileage = db.Column(db.Float, default=0)
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    driver = db.Column(db.String(100), nullable=False)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=False)
+    start_mileage = db.Column(db.Integer, nullable=False)
+    end_mileage = db.Column(db.Integer, nullable=False)
+    fuel = db.Column(db.Float, nullable=True)
+    trip_details = db.Column(db.String(255), nullable=True)
+    damage = db.Column(db.String(255), nullable=True)
+
+    vehicle = db.relationship('Vehicle', backref='trips')
